088f88b1507db1629fdb88748916157470035f42
heyunqiang <heyunqiang99@163.com>
2019-07-26 00:43:47 +0800

    Person类

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,39 @@
+/**
+ * @ClassName: Hello
+ * @Description: 欢迎来到java
+ * @author: heyunqiang
+ */
 public class Hello {
+
+    // 使用static修饰的方法，属于这个类本身。
+    // 使用static修饰的方法, 既可以使用类作为调用者，也可以使用对象
+    public static void swap(int a, int b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println("swap方法里，a=" + a + ",b=" + b);
+    }
+
+
+    public static void test(int a,String...books){
+
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("HelloJava");
+        /*
+            对象的产生 和 使用？
+            创建对象的根本途径是构造器，
+            通过new 关键字来调用某一个类的构造器
+            即可创建这个类的实例。
+         */
+        Person personOne = new Person();
+        personOne.say("Heyunqiang", 20);
+
+        int a = 1;
+        int b = 3;
+        swap(a, b); //上面int a,int b,副本 传入swap()
+        System.out.println("swap方法里，a=" + a + ",b=" + b);
+
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,29 @@
+/**
+ * All rights Reserved, Designed By www.hileader.com
+ *
+ * @version V1.0
+ * @Title: Person
+ * @Description: 怎样定义类？ 如何使用类？
+ * @author: apple
+ * @date: 2019-07-25 22:40
+ * @Copyright: 2019 www.hileader.com Inc. All rights reserved.
+ * 注意：本内容仅限于上海里德软件有限公司内部传阅，禁止外泄以及用于其他的商业目
+ */
+
+public class Person {
+    /*
+    1. 成员变量
+    2. 成员方法
+    3. 构造器
+    .....
+     */
+
+    public String name;
+    public int age;
+
+    public void say(String name,int age){
+        System.out.println("我是"+name+",今年"+age+"岁了。");
+    }
+
+
+}
